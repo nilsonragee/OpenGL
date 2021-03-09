672cb236eac3d388e22308d685d277621b698cc4
nilsoncore <mr.yaroslav2002ilin@gmail.com>
2021-03-10 00:22:49 +0300

    Project restructure

---
+++ libs/stb_image.cpp
@@ -0,0 +1,5 @@
+// We don't actually need to write code here,
+// because all the code is already written in header file.
+// All we need to do is just define STB_IMAGE_IMPLEMENTATION.
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
